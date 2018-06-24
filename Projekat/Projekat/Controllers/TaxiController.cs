cc52b67b2d6b35675cd520e6b18b302cb116d8ba
JovanDavidovic <jovanvisoki96@gmail.com>
2018-06-25 01:24:39 +0200

    tacka 6

---
+++ Projekat/Projekat/Controllers/TaxiController.cs
@@ -0,0 +1,148 @@
+﻿using Projekat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projekat.Controllers
+{
+    public class TaxiController : Controller
+    {
+        private Dictionary<string, Korisnik> getKorisnik
+        {
+            get
+            {
+                return (Dictionary<string, Korisnik>)Session["RegistrovaniKorisnici"];
+            }
+        }
+
+        private Dictionary<string, Korisnik> getAdministratori
+        {
+            get
+            {
+                return (Dictionary<string, Korisnik>)Session["Administratori"];
+            }
+        }
+
+        private Dictionary<string, Vozac> getVozaci
+        {
+            get
+            {
+                return (Dictionary<string, Vozac>)Session["Vozaci"];
+            }
+        }
+        // GET: Taxi
+        public ActionResult Index()
+        {
+            if (Session["Ulogovan"] != null)
+                return View("HomepageMusterija");
+            return View();
+        }
+
+        public ActionResult GoToSignUp()
+        {
+            return View("SignUp");
+        }
+        public ActionResult GoToLogIn()
+        {
+            return View("LogIn");
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(Korisnik k)
+        {
+            k.Uloga = ULOGA.MUSTERIJA;
+            if (getKorisnik.ContainsKey(k.KorisnickoIme) || getAdministratori.ContainsKey(k.KorisnickoIme) || getVozaci.ContainsKey(k.KorisnickoIme))
+            {
+                ViewBag.korisnik = k;
+                return View("SignUpError");
+            }
+            else
+            {
+                getKorisnik.Add(k.KorisnickoIme, k);
+            }
+            return View("Index");
+        }
+        [HttpPost]
+        public ActionResult LogIn(string korisnickoIme, string lozinka)
+        {
+            if (!getKorisnik.ContainsKey(korisnickoIme))
+            {
+                if (getAdministratori.ContainsKey(korisnickoIme))
+                {
+                    if (!getAdministratori[korisnickoIme].Lozinka.Equals(lozinka))
+                    {
+                        return View("LozinkaError");
+                    }
+                    Session["Ulogovan"] = getAdministratori[korisnickoIme];
+                    return View("HomepageAdministrator");
+                }
+                if (getVozaci.ContainsKey(korisnickoIme))
+                {
+                    if (!getVozaci[korisnickoIme].Lozinka.Equals(lozinka))
+                    {
+                        return View("LozinkaError");
+                    }
+                    Session["Ulogovan"] = getVozaci[korisnickoIme];
+                    return View("HomepageVozac");
+                }
+                return View("KorisnickoImeError");
+            }
+
+            if (!getKorisnik[korisnickoIme].Lozinka.Equals(lozinka))
+            {
+                return View("LozinkaError");
+            }
+
+            Session["Ulogovan"] = getKorisnik[korisnickoIme];
+            return View("HomepageMusterija");
+        }
+
+        public ActionResult Izmena()
+        {
+            return View("Izmena");
+        }
+
+        public ActionResult Izmeni(Korisnik k)
+        {
+            getKorisnik[k.KorisnickoIme] = k;
+            Session["Ulogovan"] = getKorisnik[k.KorisnickoIme];
+            return View("Izmena");
+        }
+
+        public ActionResult GoToKreiranjeVozaca()
+        {
+            return View("KreiranjeVozaca");
+        }
+        [HttpPost]
+        public ActionResult KreiranjeVozaca(Vozac v, Automobil a, Lokacija l, Adresa ad)
+        {
+            a.vozac = v;
+            v.automobil = a;
+            l.adresa = ad;
+            v.lokacija = l;
+
+            getVozaci.Add(v.KorisnickoIme, v);
+            return View("HomepageAdministrator");
+        }
+
+        public ActionResult PromeniLokaciju()
+        {
+            return View("PromenaLokacije");
+        }
+        [HttpPost]
+        public ActionResult PromenaLokacije(string log, Lokacija l, Adresa ad)
+        {
+            l.adresa = ad;
+            getVozaci[log].lokacija = l;
+            Session["Ulogovan"] = getVozaci[log];
+            return View("HomepageVozac");
+        }
+        public ActionResult Odjava()
+        {
+            Session["Ulogovan"] = null;
+            return View("Index");
+        }
+    }
+}
--- Projekat/Projekat/Global.asax.cs
+++ Projekat/Projekat/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Projekat.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +18,41 @@ namespace Projekat
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Session_Start()
+        {
+            HttpContext.Current.Session["RegistrovaniKorisnici"] = new Dictionary<string, Korisnik>();
+            Dictionary<string, Korisnik> administratori = new Dictionary<string, Korisnik>();
+            string admin;
+            if (!File.Exists("Administratori.txt"))
+            {
+                File.Create("Administratori.txt");
+            }
+            System.IO.StreamReader file = new System.IO.StreamReader("C:/FAX/WEB/Projekat/WP1718-PR13-2015/Projekat/packages/Administratori.txt");
+            while ((admin = file.ReadLine()) != null)
+            {
+                Korisnik administrator = new Korisnik();
+                administrator.KorisnickoIme = admin.Split(' ')[0];
+                administrator.Ime = admin.Split(' ')[1];
+                administrator.Prezime = admin.Split(' ')[2];
+                administrator.Lozinka = admin.Split(' ')[3];
+                administrator.Email = admin.Split(' ')[4];
+                administrator.Telefon = admin.Split(' ')[5];
+                administrator.JMBG = admin.Split(' ')[6];
+                administrator.Uloga = ULOGA.DISPECER;
+                if (admin.Split(' ')[7].Equals("m"))
+                {
+                    administrator.Pol = POL.MUSKI;
+                }
+                else
+                {
+                    administrator.Pol = POL.ZENSKI;
+                }
+                administratori.Add(administrator.KorisnickoIme, administrator);
+            }
+            HttpContext.Current.Session["Administratori"] = administratori;
+            HttpContext.Current.Session["Vozaci"] = new Dictionary<string, Vozac>();
         }
     }
 }
